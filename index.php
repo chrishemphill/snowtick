d6319e84a18e9c87bdbf43b9e1f78345356576b1
Chris Hemphill <chemphill@chemphill-ltm2.internal.salesforce.com>
2015-11-11 16:50:34 -0800

    first

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ echo "Hello Snow World! <br /><br />";
 $conn = pg_connect($conn_string);
 
 if (!$conn) {
-  echo "An error occurred.\n";
-  echo $conn;
+  echo "Conn:" . $conn . "<br />";
+  echo "An error occurred while connecting to DB.\n";
   exit;
 }
 
 $result = pg_query($conn, "SELECT name, ticker__c, acres__c, typ__c  FROM area__c");
 if (!$result) {
-  echo "An error occurred.\n";
+  echo "An error occurred while making query.\n";
   exit;
 }
 
