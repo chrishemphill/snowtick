26bd3190025f1513f5b4fa5175270aa5a33ee7eb
Chris Hemphill <chris@chrishemphill.com>
2017-09-11 13:50:20 -0700

    db update

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 
 function pg_connection_string_from_database_url() {
-  extract(parse_url(getenv('DATABASE_URL'));
-  return "user=$user password=$pass host=$host dbname= sslmode=require" . substr($path, 1); # <- you may want to add sslmode=require there too
+  extract(parse_url(getenv('DATABASE_URL')));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
 ?>
