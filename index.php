03ed9051d2572b60d632bb78135cda34f766b5d6
Chris Hemphill <chris@chrishemphill.com>
2017-09-12 13:45:49 -0700

    updated

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include_once 'dbconnect.php';
 
 echo "Hello Snowtick World! <br /><br />";
 
-echo "DB URL: " . getenv('DATABASE_URL');
-echo "ENV URL: " . $_ENV["DATABASE_URL"];
+echo "DB URL: " . getenv('DATABASE_URL') . "<br />";
+echo "ENV URL: " . $_ENV["DATABASE_URL"] . "<br />";
 
 $conn = pg_connect(pg_connection_string_from_database_url());
 
